851979152daf82d968a6625135ed0bf20d046d43
Fabrício Bahiense Santos <fabriciobs2000@gmail.com>
2022-09-30 20:23:59 -0300

    Now it works! Working on the features now =)

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
-const quoteText = document.querySelectorAll(".quote"),
-quoteBtn = document.querySelector("button");
+const quoteText = document.querySelector(".quote"),
+quoteBtn = document.querySelector("button"),
+authorName = document.querySelector(".name"); 
+// let firstQuote = String;
 
-
+// window.onload = async () => {
+//     let someData = await promiseOfSomeData;
+//     someData ? quoteText.innerText = firstQuote : {};
+// };
+// const promiseOfSomeData = fetch("https://cors.deno.dev/https://mfaas.deno.dev/quotes/random").then(r => r.json()).then(data => {
+//     firstQuote = data.quote;
+// });
 
 
 // random quote function
 function randomQuote(){
 
     // fetching a random quote and parsing it to js object
-    fetch("https://mfaas.herokuapp.com/api/quotes/random").then(res => res.json()).then(result =>{
-        console.log(result);
+    fetch("https://cors.deno.dev/https://mfaas.deno.dev/quotes/random").then(res => res.json()).then(result =>{
+        quoteText.innerText = result.quote;
+        authorName.innerText = result.actor;
     });
 }
 
